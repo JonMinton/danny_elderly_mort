971bdb9d936afd4b2397ac8c3908238477cb2bc1
Jonathan Minton <nate.minton@gmail.com>
2016-07-25 10:23:14 +0100

    produce some maps that sort-of work

--- scripts/mark_green_maps.R
+++ scripts/mark_green_maps.R
@@ -11,7 +11,8 @@ pacman::p_load(
   stringr, car,
   readr,  readxl,
   tidyr, dplyr,
-  ggplot2, cowplot,
+  ggplot2, 
+  rgeos, sp, maptools,
   tmap
 )
 
@@ -19,7 +20,7 @@ pacman::p_load(
 # rectangular_data --------------------------------------------------------
 
 read_csv(
-  "data/mark_green/all_data_03_15.csv",
+  "data/mark_green/all_data_03_15.csv"
 ) %>% 
   .[,-1] %>% 
   gather(key = "tmp", value = "amount", -c(1:3)) %>% 
@@ -58,61 +59,108 @@ pop_data %>%
   tm_shape(.) + 
   tm_polygons(col = "death_rate", border.col = NULL)
 
-# Now to try the same but for three years 
+
+# Example of adding border to each region
+
+region_lad_lookup <- read_excel(
+  path = "workbook/analysistoolmid2014uk/Analysis Tool mid-2014 UK.xlsx",
+  sheet = "Detailed Components EW"
+) %>% 
+  .[c(4, 7)] %>% 
+  select(
+    region = `Region Name`, la_code = `LA Code`
+  ) 
+
+append_data(
+  shp = lad_shp, data = region_lad_lookup,
+  key.shp = "CODE", key.data = "la_code",
+  ignore.duplicates = T
+              ) %>% 
+  unionSpatialPolygons(., IDs = .$region) -> region_shp
+
+# Note unionSpatialPolygons returns a SpatialPolygons object not a spatialpolygons DF
+# to convert back
+#http://gis.stackexchange.com/questions/61633/r-convert-a-spatial-polygon-objet-to-spatial-polygon-data-frame
+tmp_df <- data.frame(id = getSpPPolygonsIDSlots(region_shp))
+row.names(tmp_df) <- getSpPPolygonsIDSlots(region_shp)
+
+region_shp <- SpatialPolygonsDataFrame(region_shp, data = tmp_df)
+rm(tmp_df)
 
 pop_data %>% 
-  filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006)) %>% 
-  select(-pop, -deaths, -ratio)  %>% 
-  mutate(year = paste0("y_", year)) %>% 
-  spread(year, death_rate) %>% 
+  filter(sex == "female", age_band == "80-84", year == 2002) %>% 
   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
   tm_shape(.) + 
-  tm_polygons(col = c("y_2002", "y_2004", "y_2006"), border.col = NULL)
+  tm_polygons(col = "death_rate", border.col = NULL) + 
+  tm_shape(region_shp) + # This now works with the region_level shapefile
+  tm_borders() # this adds the border of the polygons
 
-# Different scales for each of the facets. Will look at other way of appending
+# Now if I want labels
+# pop_data %>% 
+#   filter(sex == "female", age_band == "80-84", year == 2002) %>% 
+#   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
+#   tm_shape(.) + 
+#   tm_polygons(col = "death_rate", border.col = NULL) + 
+#   tm_shape(region_shp) + 
+#   tm_text("id", shadow = T)
 
-pop_data %>% 
-  filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006)) %>% 
-  append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE", ignore.duplicates = T) %>% 
-  tm_shape(.) + tm_facets(by = "year") +
-  tm_polygons(col = "death_rate", border.col = NULL) 
-# Does not work as expected. Only shows one map rather than three
 
-# Returning to older version but with free.scales = F
-
-# That didn't work, as argument not available 
-pop_data %>% 
-  filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006, 2008)) %>% 
-  select(-pop, -deaths, -ratio)  %>% 
-  spread(year, death_rate) %>% 
-  append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
-  tm_shape(.) + 
-  tm_polygons(
-    col = c("2002", "2004", "2006", "2008"), 
-    border.col = NULL, 
-    style = "fixed", breaks = seq(0, 0.2, by = 0.025)
-  )
+# 
+# # Now to try the same but for three years 
+# 
+# pop_data %>% 
+#   filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006)) %>% 
+#   select(-pop, -deaths, -ratio)  %>% 
+#   mutate(year = paste0("y_", year)) %>% 
+#   spread(year, death_rate) %>% 
+#   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
+#   tm_shape(.) + 
+#   tm_polygons(col = c("y_2002", "y_2004", "y_2006"), border.col = NULL)
+# 
+# # Different scales for each of the facets. Will look at other way of appending
+# 
+# pop_data %>% 
+#   filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006)) %>% 
+#   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE", ignore.duplicates = T) %>% 
+#   tm_shape(.) + tm_facets(by = "year") +
+#   tm_polygons(col = "death_rate", border.col = NULL) 
+# # Does not work as expected. Only shows one map rather than three
+# 
+# # Returning to older version but with free.scales = F
+# 
+# # That didn't work, as argument not available 
+# pop_data %>% 
+#   filter(sex == "female", age_band == "80-84", year %in% c(2002, 2004, 2006, 2008)) %>% 
+#   select(-pop, -deaths, -ratio)  %>% 
+#   spread(year, death_rate) %>% 
+#   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
+#   tm_shape(.) + 
+#   tm_polygons(
+#     col = c("2002", "2004", "2006", "2008"), 
+#     border.col = NULL, 
+#     style = "fixed", breaks = seq(0, 0.2, by = 0.025)
+#   )
 
 
 # This works as proof of concept, but what's needed is..
 # rate ratio 2015-2014, faceted by gender and age group
 
 # Will look again at faceting options, otherwise produce as two separate lines 
-# to be merged together 
-
-pop_data %>% 
-  filter(year == 2015) %>% 
-  append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE", ignore.duplicates = T) %>% 
-  tm_shape(.) + 
-  tm_facets(by = c("sex", "age_band"), free.scales.fill = F) +
-  tm_polygons(
-    col = "ratio", 
-    border.col = NULL
-#    style = "fixed", breaks = seq(0, 0.2, by = 0.025)
-  )
-
-# Again, I can't get this approach to work as expected, so will bodge by producing 
-# two separate rows, one for males and the other for females 
+# # to be merged together 
+# 
+# pop_data %>% 
+#   filter(year == 2015) %>% 
+#   append_data(lad_shp, ., key.data = "lad2014_code", key.shp = "CODE", ignore.duplicates = T) %>% 
+#   tm_shape(.) + 
+#   tm_facets(by = c("sex", "age_band"), free.scales.fill = F) +
+#   tm_polygons(
+#     col = "ratio", 
+#     border.col = NULL
+# #    style = "fixed", breaks = seq(0, 0.2, by = 0.025)
+#   )
+# 
+# # Again, I can't get this approach to work as expected, so will bodge by producing 
+# # two separate rows, one for males and the other for females 
 
 pop_data %>% 
   filter(year == 2015, sex == "female") %>%
@@ -125,7 +173,14 @@ pop_data %>%
     border.col = NULL, 
     style = "fixed", breaks = c(0, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 8),
     palette = "Blues"
-  ) + tm_facets(ncol = 3) -> chor_female
+  ) + tm_facets(ncol = 3) +
+  tm_shape(region_shp) + # This now works with the region_level shapefile
+  tm_borders() -> chor_female
+
+save_tmap(chor_female,
+          filename = "maps/females_ratio.png", 
+          width = 30, height = 10, units = "cm", dpi = 300
+          )
 
 
 
@@ -140,9 +195,15 @@ pop_data %>%
     border.col = NULL, 
     style = "fixed", breaks = c(0, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 8),
     palette = "Blues"
-  ) + tm_facets(ncol = 3) -> chor_male
+  ) + 
+  tm_facets(ncol = 3) + 
+  tm_shape(region_shp) + # This now works with the region_level shapefile
+  tm_borders() -> chor_male
 
-
+save_tmap(chor_male,
+          filename = "maps/males_ratio.png", 
+          width = 30, height = 10, units = "cm", dpi = 300
+)
 
   
 
@@ -170,45 +231,42 @@ components %>%
 read_shape("shapefiles/2014_recaculated_cartogram/recaculated_2014_population_cartogram.shp",
            current.projection = "longlat") -> new_cart
 
-# Cartograms --------------------------------------------------------------
 
+# Alternative approach : using ggplot2 and fortify
 
-read_shape("shapefiles/2014 Total Pop cartogram/eng_wales_lads_cart.shp",
-           current.projection = "longlat") -> cart_shp
+new_cart_fort <- fortify(new_cart, region = "CODE")
 
+ggplot(new_cart_fort, aes(x = long, y = lat, group = id)) + 
+  geom_line(col = NA, alpha = 0.8)
+
+# Using example from Winston Chang book
+
+theme_clean <- function(base_size = 12) {
+  require(grid)
+  theme_grey(base_size) %+replace%
+    theme(
+      axis.title  = element_blank(),
+      axis.text = element_blank(),
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      axis.ticks.length = unit(0, "cm"),
+      axis.ticks.margin = unit(0, "cm"),
+      panel.margin = unit(0, "lines"),
+      plot.margin = unit(c(0, 0, 0, 0), "lines"),
+      complete = TRUE
+    )
+}
+      
+new_cart_fort  %>% ggplot(., aes(x = long, y = lat, group = id)) + 
+  geom_path() + theme_clean()
+
+# To do a choropleth
 pop_data %>% 
-  filter(year == 2015, sex == "female") %>%
+  filter(year == 2015, sex == "male") %>% # vary sex
+  filter(age_band == "85-89") %>% # vary age band
   select(-pop, -deaths, -death_rate) %>% 
-  spread(age_band, ratio) %>% 
-  append_data(cart_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
-  tm_shape(.) + 
-  tm_polygons(
-    col = c("80-84", "85-89", "90+"), 
-    border.col = NULL 
-#    style = "fixed", breaks = c(0, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 8),
-#    palette = "Blues"
-  ) + tm_facets(ncol = 3) -> cart_female
-
-
-
-pop_data %>% 
-  filter(year == 2015, sex == "male") %>%
-  mutate(pop = pop + 0.1, deaths = deaths + 0.1) %>% #continuity correction
-  select(-pop, -deaths, -death_rate) %>% 
-  spread(age_band, ratio) %>% 
-  append_data(cart_shp, ., key.data = "lad2014_code", key.shp = "CODE") %>% 
-  tm_shape(.) + 
-  tm_polygons(
-    col = c("80-84", "85-89", "90+"), 
-    border.col = NULL, 
-    style = "fixed", breaks = c(0, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 8),
-    palette = "Blues"
-  ) + tm_facets(ncol = 3) -> cart_male
-
-
-
-
-
-
+  right_join(new_cart_fort, ., by = c("id" = "lad2014_code" )) %>% tbl_df %>%  
+  ggplot(., aes(x = long, y = lat, group = id, fill = ratio)) + 
+  geom_polygon() + theme_clean()
 
 
