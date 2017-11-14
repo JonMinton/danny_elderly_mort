84e393d5c9d85564cfa163fa90fca2d819a62b59
Jonathan Minton <nate.minton@gmail.com>
2017-11-14 12:41:56 +0000

    compare ons projections

---
+++ scripts/compare_projections.R
@@ -0,0 +1,70 @@
+# Danny - difference in projections 
+
+rm(list = ls())
+
+
+pacman::p_load(
+  tidyverse,
+  readxl,
+  scales,
+  stringr
+)
+
+
+# 2014 tab
+
+proj_2014 <- read_excel(path = "data/Spreadsheet.xlsx", sheet = '2014')
+proj_2016 <- read_excel(path = "data/Spreadsheet.xlsx", sheet = '2016')
+
+proj_2014 %>% 
+  gather(key = "year", value = "deaths", `2014 - 2015`:`2113 - 2114`) %>% 
+  mutate(proj_year = 2014) %>% 
+  bind_rows(
+    proj_2016 %>% 
+      gather(key = "year", value = "deaths", `2016 - 2017`:`2115 - 2116`) %>% 
+      mutate(proj_year = 2016)
+  ) %>% 
+  select(proj_year, sex = Sex, age = Age, year, deaths) %>% 
+  mutate(age = case_when(
+    age == 'Birth' ~ '-1',
+    age == '105+' ~ '105',
+    TRUE ~ age
+  ) %>% as.numeric() ) %>% 
+  mutate(
+    year = str_extract(year, "^[0-9]{4}") %>% as.numeric() 
+  ) -> proj_joined
+
+proj_joined %>% 
+  ggplot(aes(x = year, y = age, fill = deaths)) + 
+  geom_tile() + 
+  coord_equal() +
+  scale_fill_gradientn(
+    "Deaths", 
+    colours = scales::brewer_pal(palette = "Paired")(12)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, by = 10)
+  ) + 
+  facet_grid(sex ~ proj_year)
+
+ggsave("figures/ons_projections_compared.png", width = 30, height = 30, units = "cm", dpi = 300)
+
+proj_joined %>% 
+  spread(proj_year, deaths) %>% 
+  mutate(difference = `2016` - `2014`) %>% 
+  ggplot(aes(x = year, y = age, fill = difference)) + 
+  geom_tile() + 
+  coord_equal() +
+  scale_fill_gradientn(
+    "Difference", 
+    colours = scales::brewer_pal(palette = "Paired")(12)
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 100, by = 10)
+  ) + 
+  facet_wrap(~sex)
+
+ggsave("figures/ons_projections_difference.png", width = 30, height = 15, units = "cm", dpi = 300)
+
+
+
